74926d5f0c3bc61a17c8268dec44cfbbe2109870
SRAVANTHI SINHA <sinha.sravanthi@gmail.com>
2024-07-29 17:58:40 -0700

    basic dockerized setup for front end

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Quizler</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
